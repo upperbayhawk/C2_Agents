7ee4fbd11da158994abbf4f0d0ee675adeb2ca8b
Dave Hardin <dave@upperbay.com>
2024-02-27 16:09:51 -0500

    edits

--- Source/Upperbay/Core/Library/MyAppConfig .cs
+++ Source/Upperbay/Core/Library/MyAppConfig .cs
@@ -61,7 +61,7 @@ namespace Upperbay.Core.Library
             {
                 //System Parameters
                 //case "ClusterName": val = "BETA1"; break;
-                case "ClusterName": val = "VOLTA_BETA2"; break;
+                case "ClusterName": val = "VOLTA_BETA2_HIVE"; break;
                 case "ClusterVersion": val = "0.6.9"; break;
                 //
                 case "Period": val = "5000"; break;
@@ -263,6 +263,30 @@ namespace Upperbay.Core.Library
                 VOLTA_BETA2.Add("LMPKey", "312249d38ae6410bbd6ea56f8343eef8");
                 VOLTA_BETA2.Add("LMPNode", "49955");
                 _clusterParameters.Add(cluster, VOLTA_BETA2);
+
+                cluster = "VOLTA_BETA2_HIVE";
+                Dictionary<string, string> VOLTA_BETA2_HIVE = new Dictionary<string, string>();
+                VOLTA_BETA2_HIVE.Add("EthereumContractAddress", "0xaB5B2Cf8D5ba4e58Dd7c1aE3799d6c1a642d2E6a");
+                VOLTA_BETA2_HIVE.Add("RemoteEthereumServerURL", "http://www.mauiview.com:8545");
+                VOLTA_BETA2_HIVE.Add("EthereumChainId", "73799");
+                VOLTA_BETA2_HIVE.Add("EthereumClusterKey", "0x0949a8d20891952dbc52ec59a2aaf36dcd97b5a114103ba4c949fdc0652a2a7f");
+                VOLTA_BETA2_HIVE.Add("EthereumClusterAddress", "0x47b03cb6a335A15a87Fb63AE295add5aFB0539ed");
+                VOLTA_BETA2_HIVE.Add("MqttCloudSecureLoginName", "pearlygates");
+                VOLTA_BETA2_HIVE.Add("MqttCloudSecurePassword", "!Pearlygates87!");
+                VOLTA_BETA2_HIVE.Add("MqttCloudSecureIpAddress", "broker.hivemq.com");
+                VOLTA_BETA2_HIVE.Add("MqttCloudSecurePort", "1883");
+                VOLTA_BETA2_HIVE.Add("MqttCloudIpAddress", "broker.hivemq.com");
+                VOLTA_BETA2_HIVE.Add("MqttCloudPort", "1883");
+                //VOLTA_BETA2_HIVE.Add("MqttCloudSecureIpAddress", "9c41862892f042dea951b2f348b676ec.s1.eu.hivemq.cloud");
+                //VOLTA_BETA2_HIVE.Add("MqttCloudSecurePort", "8883");
+                //VOLTA_BETA2_HIVE.Add("MqttCloudIpAddress", "9c41862892f042dea951b2f348b676ec.s1.eu.hivemq.cloud");
+                //VOLTA_BETA2_HIVE.Add("MqttCloudPort", "8883");
+                VOLTA_BETA2_HIVE.Add("SMSAccountName", "davidhardin2");
+                VOLTA_BETA2_HIVE.Add("SMSAccountKey", "eWymWS3pHCqvewP8NqKdc2DnvCABDE");
+                VOLTA_BETA2_HIVE.Add("LMPRTO", "PJM");
+                VOLTA_BETA2_HIVE.Add("LMPKey", "312249d38ae6410bbd6ea56f8343eef8");
+                VOLTA_BETA2_HIVE.Add("LMPNode", "49955");
+                _clusterParameters.Add(cluster, VOLTA_BETA2_HIVE);
 
                 cluster = "VOLTA_BETA3";
                 Dictionary<string, string> VOLTA_BETA3 = new Dictionary<string, string>();
